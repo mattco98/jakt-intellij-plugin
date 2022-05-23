fb93fb823ce129de9a0660b8912a3644fcf64dae
Matthew Olsson <matthewcolsson@gmail.com>
2022-05-23 04:50:25 -0700

    gitignore: Add testData directory

--- src/test/kotlin/org/serenityos/jakt/JaktParserTest.kt
+++ src/test/kotlin/org/serenityos/jakt/JaktParserTest.kt
@@ -2,6 +2,10 @@ package org.serenityos.jakt
 
 import com.intellij.testFramework.ParsingTestCase
 
+/**
+ * Requires a file at src/main/resources/testData/ParsingTestData.jakt with
+ * code to be ran through the parser.
+ */
 class JaktParserTest : ParsingTestCase("", "jakt", JaktParserDefinition()) {
     override fun getTestDataPath(): String {
         return "src/main/resources/testData"
