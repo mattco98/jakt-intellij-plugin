ba31b13b3bf354201ec219fce28ee7cbd30d62cb
Matthew Olsson <matthewcolsson@gmail.com>
2022-05-25 09:44:21 -0700

    SyntaxHighlighter: Add more colors

--- src/main/kotlin/org/serenityos/jakt/JaktColorSettingsPage.kt
+++ src/main/kotlin/org/serenityos/jakt/JaktColorSettingsPage.kt
@@ -34,19 +34,38 @@ class JaktColorSettingsPage : ColorSettingsPage {
 
     companion object {
         private val DESCRIPTORS = mapOf(
-            "Numbers" to HIGHLIGHT_NUMBER,
-            "Comments" to HIGHLIGHT_COMMENT,
-            "Strings and Characters" to HIGHLIGHT_STRING,
-            "Identifiers" to HIGHLIGHT_IDENTIFIER,
-            "Keywords" to HIGHLIGHT_KEYWORD,
+            "Identifiers" to Highlights.IDENTIFIER,
+            "Comments" to Highlights.COMMENT,
 
-            "Delimiters//Braces" to HIGHLIGHT_BRACES,
-            "Delimiters//Brackets" to HIGHLIGHT_BRACKETS,
-            "Delimiters//Parenthesis" to HIGHLIGHT_PARENTHESES,
+            "Literals//Numbers" to Highlights.LITERAL_NUMBER,
+            "Literals//Strings and Characters" to Highlights.LITERAL_STRING,
+            "Literals//Booleans" to Highlights.LITERAL_BOOLEAN,
+            "Literals//Arrays" to Highlights.LITERAL_ARRAY,
+            "Literals//Dictionaries" to Highlights.LITERAL_DICTIONARY,
+            "Literals//Sets" to Highlights.LITERAL_SET,
 
-            "Operators//Dot" to HIGHLIGHT_DOT,
-            "Operators//Comma" to HIGHLIGHT_COMMA,
-            "Operators//Other" to HIGHLIGHT_OPERATION_SIGN,
+            "Delimiters//Braces" to Highlights.DELIMITER_BRACE,
+            "Delimiters//Brackets" to Highlights.DELIMITER_BRACKET,
+            "Delimiters//Parenthesis" to Highlights.DELIMITER_PARENTHESIS,
+
+            "Keywords//Base" to Highlights.KEYWORD_BASE,
+            "Keywords//Declaration" to Highlights.KEYWORD_DECLARATION,
+            "Keywords//Visibility" to Highlights.KEYWORD_VISIBILITY,
+            "Keywords//Control Flow" to Highlights.KEYWORD_CONTROL_FLOW,
+            "Keywords//Modifiers" to Highlights.KEYWORD_MODIFIER,
+            "Keywords//unsafe and cpp" to Highlights.KEYWORD_UNSAFE,
+
+            "Operators//Base" to Highlights.OPERATOR_BASE,
+            "Operators//Dot" to Highlights.DOT,
+            "Operators//Range" to Highlights.RANGE,
+            "Operators//Comma" to Highlights.COMMA,
+            "Operators//Colon" to Highlights.COLON,
+            "Operators//Namespace" to Highlights.NAMESPACE,
+            "Operators//Semicolon" to Highlights.SEMICOLON,
+            "Operators//Arrow" to Highlights.ARROW,
+            "Operators//Arithmetic Operators" to Highlights.ARITHMETIC_OPERATOR,
+            "Operators//Bitwise Operators" to Highlights.BITWISE_OPERATOR,
+            "Operators//Other" to Highlights.BITWISE_OPERATOR,
         ).map { AttributesDescriptor(it.key, it.value) }.toTypedArray()
     }
 }
--- src/main/kotlin/org/serenityos/jakt/JaktSyntaxHighlighter.kt
+++ src/main/kotlin/org/serenityos/jakt/JaktSyntaxHighlighter.kt
@@ -1,9 +1,8 @@
 package org.serenityos.jakt
 
 import com.intellij.lexer.Lexer
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as Default
 import com.intellij.openapi.editor.colors.TextAttributesKey
-import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import com.intellij.openapi.project.Project
@@ -11,79 +10,117 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.tree.IElementType
 import org.serenityos.jakt.JaktTypes.*
 
-val HIGHLIGHT_DOT = createTextAttributesKey("JAKT_DOT", DefaultLanguageHighlighterColors.DOT)
-val HIGHLIGHT_COMMA = createTextAttributesKey("JAKT_COMMA", DefaultLanguageHighlighterColors.COMMA)
-val HIGHLIGHT_IDENTIFIER = createTextAttributesKey("JAKT_IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER)
-val HIGHLIGHT_STRING = createTextAttributesKey("JAKT_STRING", DefaultLanguageHighlighterColors.STRING)
-val HIGHLIGHT_OPERATION_SIGN = createTextAttributesKey("JAKT_OPERATION_SIGN", DefaultLanguageHighlighterColors.OPERATION_SIGN)
-val HIGHLIGHT_KEYWORD = createTextAttributesKey("JAKT_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD)
-val HIGHLIGHT_COMMENT = createTextAttributesKey("JAKT_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
-val HIGHLIGHT_NUMBER = createTextAttributesKey("JAKT_NUMBER", DefaultLanguageHighlighterColors.NUMBER)
+object Highlights {
+    val IDENTIFIER = Default.IDENTIFIER.extend("IDENTIFIER")
+    val COMMENT = Default.LINE_COMMENT.extend("COMMENT")
 
-val HIGHLIGHT_PARENTHESES = createTextAttributesKey("JAKT_PARENTHESES", DefaultLanguageHighlighterColors.PARENTHESES)
-val HIGHLIGHT_BRACES = createTextAttributesKey("JAKT_BRACES", DefaultLanguageHighlighterColors.BRACES)
-val HIGHLIGHT_BRACKETS = createTextAttributesKey("JAKT_BRACKETS", DefaultLanguageHighlighterColors.BRACKETS)
+    val LITERAL_NUMBER = Default.NUMBER.extend("LITERAL_NUMBER")
+    val LITERAL_STRING = Default.STRING.extend("LITERAL_STRING")
+    val LITERAL_BOOLEAN = Default.NUMBER.extend("LITERAL_BOOLEAN")
+    val LITERAL_ARRAY = Default.BRACKETS.extend("LITERAL_ARRAY")
+    val LITERAL_DICTIONARY = Default.BRACKETS.extend("LITERAL_DICTIONARY")
+    val LITERAL_SET = Default.BRACES.extend("LITERAL_SET")
+
+    val DELIMITER_PARENTHESIS = Default.PARENTHESES.extend("DELIMITER_PARENTHESES")
+    val DELIMITER_BRACE = Default.BRACES.extend("DELIMITER_BRACE")
+    val DELIMITER_BRACKET = Default.BRACKETS.extend("DELIMITER_BRACKET")
+
+    val KEYWORD_BASE = Default.KEYWORD.extend("KEYWORD_BASE")
+    val KEYWORD_VISIBILITY = KEYWORD_BASE.extend("KEYWORD_VISIBILITY")
+    val KEYWORD_DECLARATION = KEYWORD_BASE.extend("KEYWORD_DECLARATION")
+    val KEYWORD_CONTROL_FLOW = KEYWORD_BASE.extend("KEYWORD_CONTROL_FLOW")
+    val KEYWORD_MODIFIER = KEYWORD_BASE.extend("KEYWORD_MODIFIER")
+    val KEYWORD_UNSAFE = KEYWORD_BASE.extend("KEYWORD_UNSAFE")
+
+    val OPERATOR_BASE = Default.OPERATION_SIGN.extend("OPERATOR_BASE")
+    val DOT = OPERATOR_BASE.extend("DOT")
+    val RANGE = OPERATOR_BASE.extend("RANGE")
+    val COMMA = OPERATOR_BASE.extend("COMMA")
+    val COLON = OPERATOR_BASE.extend("COLON")
+    val NAMESPACE = OPERATOR_BASE.extend("NAMESPACE")
+    val SEMICOLON = OPERATOR_BASE.extend("SEMICOLON")
+    val ARITHMETIC_OPERATOR = OPERATOR_BASE.extend("BINARY_OPERATOR")
+    val BITWISE_OPERATOR = OPERATOR_BASE.extend("BITWISE_OPERATOR")
+    val COMPARISON_OPERATOR = OPERATOR_BASE.extend("COMPARISON_OPERATOR")
+    val OTHER_OPERATOR = OPERATOR_BASE.extend("OTHER_OPERATOR")
+    val ARROW = OPERATOR_BASE.extend("ARROW")
+
+    private fun TextAttributesKey.extend(name: String) = TextAttributesKey.createTextAttributesKey("JAKT_$name", this)
+}
 
 class JaktSyntaxHighlighter : SyntaxHighlighterBase() {
     override fun getHighlightingLexer(): Lexer = JaktLexerAdapter()
 
     override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> {
         return when (tokenType) {
-            EXTERN_KEYWORD,
+            IDENTIFIER -> Highlights.IDENTIFIER
+            COMMENT -> Highlights.COMMENT
 
+            NUMERIC_LITERAL -> Highlights.LITERAL_NUMBER
+            STRING_LITERAL -> Highlights.LITERAL_STRING
+            BOOLEAN_LITERAL -> Highlights.LITERAL_BOOLEAN
+            ARRAY_EXPRESSION -> Highlights.LITERAL_ARRAY
+            DICTIONARY_EXPRESSION -> Highlights.LITERAL_DICTIONARY
+            SET_EXPRESSION -> Highlights.LITERAL_SET
+
+            PAREN_OPEN, PAREN_CLOSE -> Highlights.DELIMITER_PARENTHESIS
+            CURLY_OPEN, CURLY_CLOSE -> Highlights.DELIMITER_BRACE
+            BRACKET_OPEN, BRACKET_CLOSE -> Highlights.DELIMITER_BRACKET
+
+            EXTERN_KEYWORD,
             CLASS_KEYWORD,
             STRUCT_KEYWORD,
             FUNCTION_KEYWORD,
+            LET_KEYWORD -> Highlights.KEYWORD_DECLARATION
 
-            LET_KEYWORD,
+            // RESTRICTED_KEYWORD,
+            PRIVATE_KEYWORD,
+            PUBLIC_KEYWORD -> Highlights.KEYWORD_VISIBILITY
+
             IF_KEYWORD,
             ELSE_KEYWORD,
-
             WHILE_KEYWORD,
             FOR_KEYWORD,
             LOOP_KEYWORD,
-
             RETURN_KEYWORD,
             THROW_KEYWORD,
-            DEFER_KEYWORD,
-
-            TRUE_KEYWORD,
-            FALSE_KEYWORD,
+            DEFER_KEYWORD -> Highlights.KEYWORD_CONTROL_FLOW
 
             MUTABLE_KEYWORD,
             ANONYMOUS_KEYWORD,
             RAW_KEYWORD,
-            THROWS_KEYWORD -> HIGHLIGHT_KEYWORD
+            THROWS_KEYWORD -> Highlights.KEYWORD_MODIFIER
 
-            PAREN_OPEN,
-            PAREN_CLOSE -> HIGHLIGHT_PARENTHESES
+            UNSAFE_KEYWORD, CPP_KEYWORD -> Highlights.KEYWORD_UNSAFE
 
-            CURLY_OPEN,
-            CURLY_CLOSE -> HIGHLIGHT_BRACES
+            DOT -> Highlights.DOT
+            DOT_DOT -> Highlights.RANGE
+            COMMA -> Highlights.COMMA
+            COLON -> Highlights.COLON
+            COLON_COLON -> Highlights.NAMESPACE
+            SEMICOLON -> Highlights.SEMICOLON
+            ARROW, FAT_ARROW -> Highlights.ARROW
 
-            BRACKET_OPEN,
-            BRACKET_CLOSE -> HIGHLIGHT_BRACKETS
+            PLUS, MINUS,
+            ASTERISK, SLASH, PERCENT -> Highlights.ARITHMETIC_OPERATOR
 
-            COLON,
-            COLON_COLON,
-            DOT,
-            DOT_DOT -> HIGHLIGHT_DOT
+            LEFT_SHIFT, RIGHT_SHIFT,
+            ARITH_LEFT_SHIFT, ARITH_RIGHT_SHIFT,
+            AMPERSAND, PIPE, CARET -> Highlights.BITWISE_OPERATOR
 
-            COMMA -> HIGHLIGHT_COMMA
+            DOUBLE_EQUALS, NOT_EQUALS,
+            LESS_THAN, LESS_THAN_EQUALS,
+            GREATER_THAN, GREATER_THAN_EQUALS,
+            AND, OR -> Highlights.COMPARISON_OPERATOR
 
             EQUALS,
-            PLUS,
-            MINUS,
-            ASTERISK,
-            SLASH,
-            AMPERSAND -> HIGHLIGHT_OPERATION_SIGN
+            QUESTION_MARK, DOUBLE_QUESTION_MARK -> Highlights.OTHER_OPERATOR
 
-            IDENTIFIER -> HIGHLIGHT_IDENTIFIER
-
-            STRING_LITERAL -> HIGHLIGHT_STRING
-            COMMENT -> HIGHLIGHT_COMMENT
-
-            else -> return emptyArray()
+            else -> {
+                if (tokenType is JaktElement)
+                    println("No Token highlighting defined for element ${tokenType::class.simpleName}!")
+                return emptyArray()
+            }
         }.let { arrayOf(it) }
     }
 }
