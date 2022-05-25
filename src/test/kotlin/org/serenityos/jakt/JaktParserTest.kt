88713a55900bf32fb84c6027a94dc777089023ed
Matthew Olsson <matthewcolsson@gmail.com>
2022-05-25 09:34:48 -0700

    JaktParserTest: Remove output file before starting

--- src/test/kotlin/org/serenityos/jakt/JaktParserTest.kt
+++ src/test/kotlin/org/serenityos/jakt/JaktParserTest.kt
@@ -1,12 +1,17 @@
 package org.serenityos.jakt
 
 import com.intellij.testFramework.ParsingTestCase
+import java.io.File
 
 /**
  * Requires a file at src/main/resources/testData/ParsingTestData.jakt with
  * code to be ran through the parser.
  */
 class JaktParserTest : ParsingTestCase("", "jakt", JaktParserDefinition()) {
+    init {
+        File("src/main/resources/testData/ParsingTestData.txt").delete()
+    }
+
     override fun getTestDataPath(): String {
         return "src/main/resources/testData"
     }
