f124868d083ae5d448f8ed57e3572b40e945678d
Matthew Olsson <matthewcolsson@gmail.com>
2022-05-26 19:21:19 -0700

    Grammar: Improve qualifier rules; add tuple/array indexing expressions

--- src/main/kotlin/org/serenityos/jakt/plugin/annotations/JaktAnnotatorImpl.kt
+++ src/main/kotlin/org/serenityos/jakt/plugin/annotations/JaktAnnotatorImpl.kt
@@ -3,11 +3,11 @@ package org.serenityos.jakt.plugin.annotations
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
 import com.intellij.refactoring.suggested.endOffset
 import com.intellij.refactoring.suggested.startOffset
-import org.intellij.sdk.language.psi.JaktCallExpression
-import org.intellij.sdk.language.psi.JaktFunctionDeclaration
-import org.intellij.sdk.language.psi.JaktLabeledArgument
+import org.intellij.sdk.language.psi.*
+import org.serenityos.jakt.JaktTypes
 import org.serenityos.jakt.plugin.Highlights
 
 // TODO: Split this up into multiple classes when this class starts to get too complex
@@ -19,24 +19,29 @@ object JaktAnnotatorImpl : JaktAnnotator() {
                 .range(element.identifier!!)
                 .textAttributes(Highlights.FUNCTION_DECLARATION)
                 .create()
-            is JaktCallExpression -> {
-                val target = element.qualifier
-                val ident = target.plainMemberQualifier?.identifier
-                    ?: target.memberQualifier?.lastChild
-                    ?: target.namespacedMemberQualifier?.lastChild
-
-                if (ident != null) {
-                    holder.newAnnotation(HighlightSeverity.INFORMATION)
-                        .range(ident)
-                        .textAttributes(Highlights.FUNCTION_CALL)
-                        .create()
-                }
+            is JaktCallExpression -> getCallHighlightTarget(element.firstChild)?.let {
+                holder.newAnnotation(HighlightSeverity.INFORMATION)
+                    .range(it)
+                    .textAttributes(Highlights.FUNCTION_CALL)
+                    .create()
             }
             is JaktLabeledArgument -> holder
                 .newAnnotation(HighlightSeverity.INFORMATION)
                 .range(TextRange.create(element.identifier.startOffset, element.colon.endOffset))
                 .textAttributes(Highlights.FUNCTION_PARAMETER_LABEL)
                 .create()
+        }
+    }
+
+    private fun getCallHighlightTarget(expr: PsiElement): PsiElement? {
+        return when (expr) {
+            is JaktIndexedAccessExpression -> expr.bracketOpen.nextSibling
+            is JaktMemberAccessExpression -> expr.lastChild
+            is JaktNamespacedQualifier -> expr.lastChild
+            is JaktPlainQualifier -> expr.identifier
+            else -> if (expr.elementType == JaktTypes.IDENTIFIER) {
+                expr
+            } else null
         }
     }
 }
