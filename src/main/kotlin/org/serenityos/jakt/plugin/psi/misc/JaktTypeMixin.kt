7ef08cf73729e559bb15906d2018093efe5f8dd0
Matthew Olsson <matthewcolsson@gmail.com>
2022-06-05 09:19:55 -0700

    Grammar: Overhaul type definitions
    They are now properly restricted, and do not allow bogus types like
    Foo::[String]::Bar.

--- src/main/kotlin/org/serenityos/jakt/plugin/psi/misc/JaktTypeMixin.kt
+++ src/main/kotlin/org/serenityos/jakt/plugin/psi/misc/JaktTypeMixin.kt
@@ -4,9 +4,9 @@ import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.elementType
 import org.intellij.sdk.language.psi.JaktArrayType
 import org.intellij.sdk.language.psi.JaktDictionaryType
-import org.intellij.sdk.language.psi.JaktNamespacedType
 import org.intellij.sdk.language.psi.JaktOptionalType
 import org.intellij.sdk.language.psi.JaktPlainType
 import org.intellij.sdk.language.psi.JaktRawType
@@ -14,6 +14,7 @@ import org.intellij.sdk.language.psi.JaktSetType
 import org.intellij.sdk.language.psi.JaktTupleType
 import org.intellij.sdk.language.psi.JaktTypeAnnotation
 import org.intellij.sdk.language.psi.JaktWeakType
+import org.serenityos.jakt.JaktTypes
 import org.serenityos.jakt.plugin.psi.api.JaktTypeable
 import org.serenityos.jakt.plugin.psi.api.containingScope
 import org.serenityos.jakt.plugin.type.Type
@@ -26,11 +27,10 @@ abstract class JaktTypeMixin(node: ASTNode) : ASTWrapperPsiElement(node), JaktTy
                 is JaktWeakType -> Type.Weak(type!!.jaktType)
                 is JaktPlainType -> resolvePlainType(this)
                 is JaktOptionalType -> Type.Optional(type.jaktType)
-                is JaktArrayType -> Type.Array(type!!.jaktType)
+                is JaktArrayType -> Type.Array(type.jaktType)
                 is JaktDictionaryType -> Type.Dictionary(typeList[0].jaktType, typeList[1].jaktType)
                 is JaktSetType -> Type.Set(type!!.jaktType)
                 is JaktTupleType -> Type.Tuple(typeList.map { it.jaktType })
-                is JaktNamespacedType -> Type.Namespaced(typeList[0].jaktType, typeList[1].jaktType)
                 is JaktTypeAnnotation -> type.jaktType
                 else -> error("Unknown Type class ${this::class.simpleName}")
             }
@@ -41,11 +41,12 @@ abstract class JaktTypeMixin(node: ASTNode) : ASTWrapperPsiElement(node), JaktTy
 
     companion object {
         private fun resolvePlainType(element: JaktPlainType): Type {
-            val name = element.identifier.text
-
-            require(element.genericBounds == null) {
-                "expected PlainType in expression context to have no genericBounds element"
+            val idents = element.children.filter { it.elementType == JaktTypes.IDENTIFIER }
+            require(idents.size == 1) {
+                "TODO: Resolve namespace types"
             }
+
+            val name = idents.last().text
 
             val primitive = Type.Primitive.values().find { it.typeRepr() == name }
             if (primitive != null)
