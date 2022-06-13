9b8dff49614315c6296a1c250219779129a0685b
Matthew Olsson <matthewcolsson@gmail.com>
2022-06-12 17:12:11 -0700

    Resolution: Resolve aliased imports

--- src/main/kotlin/org/serenityos/jakt/plugin/psi/misc/JaktImportStatementMixin.kt
+++ src/main/kotlin/org/serenityos/jakt/plugin/psi/misc/JaktImportStatementMixin.kt
@@ -5,6 +5,7 @@ import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import org.intellij.sdk.language.psi.JaktImportStatement
 import org.intellij.sdk.language.psi.impl.JaktTopLevelDefinitionImpl
+import org.serenityos.jakt.JaktTypes
 import org.serenityos.jakt.plugin.JaktFile
 import org.serenityos.jakt.plugin.project.jaktProject
 import org.serenityos.jakt.plugin.psi.JaktPsiElement
@@ -12,16 +13,20 @@ import org.serenityos.jakt.plugin.psi.JaktPsiFactory
 import org.serenityos.jakt.plugin.psi.api.JaktTypeable
 import org.serenityos.jakt.plugin.psi.reference.JaktRef
 import org.serenityos.jakt.plugin.type.Type
+import org.serenityos.jakt.utils.findChildrenOfType
 
 abstract class JaktImportStatementMixin(
     node: ASTNode,
 ) : JaktTopLevelDefinitionImpl(node), JaktImportStatement {
+    private val nameIdent = findChildrenOfType(JaktTypes.IDENTIFIER).first()
+    private val aliasIdent = findChildrenOfType(JaktTypes.IDENTIFIER).getOrNull(1)
+
     override val jaktType: Type
         get() = CachedValuesManager.getCachedValue(this, JaktTypeable.TYPE_KEY) {
             CachedValueProvider.Result(resolveFile()?.jaktType ?: Type.Unknown, this)
         }
 
-    override fun getNameIdentifier() = identifier
+    override fun getNameIdentifier() = aliasIdent ?: nameIdent
 
     override fun getName(): String = nameIdentifier.text
 
@@ -29,7 +34,7 @@ abstract class JaktImportStatementMixin(
         nameIdentifier.replace(JaktPsiFactory(project).createIdentifier(name))
     }
 
-    fun resolveFile(): JaktFile? = jaktProject.resolveImportedFile(containingFile.virtualFile, name)
+    fun resolveFile(): JaktFile? = jaktProject.resolveImportedFile(containingFile.virtualFile, nameIdent.text)
 
     override fun getReference() = Ref(this)
 
