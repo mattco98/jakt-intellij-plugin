07e9cf0568036f4493fc67eb34c301c414c6f35b
Matthew Olsson <matthewcolsson@gmail.com>
2022-07-10 18:06:17 -0700

    Psi: Type DestructuringBinding

--- src/main/kotlin/org/serenityos/jakt/psi/declaration/JaktDestructuringBindingMixin.kt
+++ src/main/kotlin/org/serenityos/jakt/psi/declaration/JaktDestructuringBindingMixin.kt
@@ -2,7 +2,11 @@ package org.serenityos.jakt.psi.declaration
 
 import com.intellij.lang.ASTNode
 import org.intellij.sdk.language.psi.JaktDestructuringBinding
+import org.intellij.sdk.language.psi.JaktDestructuringPart
+import org.intellij.sdk.language.psi.JaktMatchPattern
+import org.serenityos.jakt.psi.caching.typeCache
 import org.serenityos.jakt.psi.named.JaktNamedElement
+import org.serenityos.jakt.type.EnumVariantType
 import org.serenityos.jakt.type.Type
 import org.serenityos.jakt.type.UnknownType
 
@@ -10,5 +14,23 @@ abstract class JaktDestructuringBindingMixin(
     node: ASTNode,
 ) : JaktNamedElement(node), JaktDestructuringBinding {
     override val jaktType: Type
-        get() = UnknownType // TODO
+        get() = typeCache().resolveWithCaching(this) {
+            val part = parent as? JaktDestructuringPart ?: return@resolveWithCaching UnknownType
+            val pattern = part.parent as? JaktMatchPattern ?: return@resolveWithCaching UnknownType
+
+            val qualifier = pattern.plainQualifierExpr.plainQualifier
+            val members = (qualifier.jaktType as? EnumVariantType)?.members ?: return@resolveWithCaching UnknownType
+
+            val previousLabel = part.destructuringLabel?.name
+
+            val matchingMembers = if (previousLabel != null) {
+                members.find { it.first == previousLabel }
+            } else {
+                members.getOrNull(pattern.destructuringPartList.indexOfFirst {
+                    it.destructuringBinding == this
+                })
+            }
+
+            matchingMembers?.second ?: UnknownType
+        }
 }
