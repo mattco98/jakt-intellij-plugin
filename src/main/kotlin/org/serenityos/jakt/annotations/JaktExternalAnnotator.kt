78ae09c2a8572c2a3d08e70ae86cec6ebab68fd7
Matthew Olsson <matthewcolsson@gmail.com>
2022-07-30 10:13:10 -0700

    ExternalAnnotator: Update flags for selfhost; process multiple errors

--- src/main/kotlin/org/serenityos/jakt/annotations/JaktExternalAnnotator.kt
+++ src/main/kotlin/org/serenityos/jakt/annotations/JaktExternalAnnotator.kt
@@ -14,12 +14,12 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.serenityos.jakt.project.jaktProject
 
-class JaktExternalAnnotator : ExternalAnnotator<PsiFile, JaktExternalAnnotator.TypecheckResult?>() {
+class JaktExternalAnnotator : ExternalAnnotator<PsiFile, List<JaktExternalAnnotator.TypecheckResult>>() {
     override fun collectInformation(file: PsiFile) = file
 
-    override fun doAnnotate(file: PsiFile): TypecheckResult? {
+    override fun doAnnotate(file: PsiFile): List<TypecheckResult> {
         if (ApplicationManager.getApplication().isUnitTestMode)
-            return null
+            return emptyList()
 
         // Ensure file is flushed to file system
         val fileDocumentManager = FileDocumentManager.getInstance()
@@ -31,29 +31,36 @@ class JaktExternalAnnotator : ExternalAnnotator<PsiFile, JaktExternalAnnotator.T
 
         val binaryLocation = file.jaktProject.jaktBinary
         if (binaryLocation?.exists() != true)
-            return null
+            return emptyList()
 
-        val filePath = file.originalFile.virtualFile?.toNioPath()?.toFile() ?: return null
+        val filePath = file.originalFile.virtualFile?.toNioPath()?.toFile() ?: return emptyList()
 
-        val output = Runtime.getRuntime().exec("${binaryLocation.absolutePath} -jc ${filePath.absolutePath}")
+        val output = Runtime.getRuntime().exec("${binaryLocation.absolutePath} -j -c ${filePath.absolutePath}")
             .inputStream.bufferedReader().readText()
 
         if (output.isBlank())
-            return null
+            return emptyList()
 
-        // TODO: Do something with the type hint?
-        return Json.Default.decodeFromString<TypecheckResult>(output.lines().firstOrNull() ?: return null)
+        return output.lines().mapNotNull { text ->
+            try {
+                Json.Default.decodeFromString<TypecheckResult>(text).takeIf { it.type == "diagnostic" }
+            } catch (e: Throwable) {
+                null
+            }
+        }
     }
 
-    override fun apply(file: PsiFile, result: TypecheckResult?, holder: AnnotationHolder) {
-        if (result == null || result.fileId != 1)
-            return
+    override fun apply(file: PsiFile, results: List<TypecheckResult>, holder: AnnotationHolder) {
+        for (result in results) {
+            if (result.fileId != 1)
+                continue
 
-        val span = result.span
-        holder.newAnnotation(HighlightSeverity.ERROR, result.message)
-            .range(TextRange.from(span.start, span.end - span.start))
-            .needsUpdateOnTyping()
-            .create()
+            val span = result.span
+            holder.newAnnotation(HighlightSeverity.ERROR, result.message)
+                .range(TextRange.from(span.start, span.end - span.start))
+                .needsUpdateOnTyping()
+                .create()
+        }
     }
 
     @Serializable
