f135c12b152d557c1fec7b08f531e2e9453c95b8
Matthew Olsson <matthewcolsson@gmail.com>
2022-08-01 18:15:17 -0700

    InlayHints: Show generics for specialized types

--- src/main/kotlin/org/serenityos/jakt/codeInsight/JaktInlayHintsProvider.kt
+++ src/main/kotlin/org/serenityos/jakt/codeInsight/JaktInlayHintsProvider.kt
@@ -2,6 +2,7 @@ package org.serenityos.jakt.codeInsight
 
 import com.intellij.codeInsight.hints.*
 import com.intellij.codeInsight.hints.presentation.InlayPresentation
+import com.intellij.codeInsight.hints.presentation.PresentationFactory
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.editor.Editor
 import com.intellij.pom.Navigatable
@@ -15,6 +16,7 @@ import org.intellij.sdk.language.psi.*
 import org.serenityos.jakt.JaktTypes.*
 import org.serenityos.jakt.annotations.JaktAnnotator
 import org.serenityos.jakt.psi.ancestorOfType
+import org.serenityos.jakt.psi.declaration.JaktGeneric
 import org.serenityos.jakt.type.*
 import javax.swing.JPanel
 
@@ -136,35 +138,77 @@ class JaktInlayHintsProvider : InlayHintsProvider<JaktInlayHintsProvider.Setting
                         hintFor(specializedType, specializations)
                     } else text(type.name).withPsiReference(type)
                 }
-                is StructType -> text(type.name).withPsiReference(type)
-                is EnumType -> text(type.name).withPsiReference(type)
+                is StructType -> {
+                    val elements = mutableListOf(text(type.name).withPsiReference(type))
+
+                    if (type.typeParameters.isNotEmpty())
+                        elements.add(genericTypeHints(type, specializations))
+
+                    seq(*elements.toTypedArray())
+                }
+                is EnumType -> {
+                    val elements = mutableListOf(text(type.name).withPsiReference(type))
+
+                    if (type.typeParameters.isNotEmpty())
+                        elements.add(genericTypeHints(type, specializations))
+
+                    seq(*elements.toTypedArray())
+                }
                 is EnumVariantType -> seq(
                     text(type.parent.name).withPsiReference(type.parent),
                     text("::"),
                     text(type.name).withPsiReference(type),
                 )
-                is FunctionType -> seq(
-                    text("function"),
-                    collapsible(
-                        prefix = text("("),
-                        collapsed = text(".."),
-                        expanded = {
-                            join(type.parameters.map {
-                                seq(
-                                    text("${it.name}: "),
-                                    hintFor(it.type, specializations)
-                                )
-                            }.ifEmpty { listOf(text("")) }, separator = { text(", ") })
-                        },
-                        suffix = text(")"),
-                        startWithPlaceholder = settings.collapseParams,
+                is FunctionType -> {
+                    val elements = mutableListOf(text("function"))
+
+                    if (type.typeParameters.isNotEmpty())
+                        elements.add(genericTypeHints(type, specializations))
+
+                    elements.add(
+                        collapsible(
+                            prefix = text("("),
+                            collapsed = text(".."),
+                            expanded = {
+                                join(type.parameters.map {
+                                    seq(
+                                        text("${it.name}: "),
+                                        hintFor(it.type, specializations)
+                                    )
+                                }.ifEmpty { listOf(text("")) }, separator = { text(", ") })
+                            },
+                            suffix = text(")"),
+                            startWithPlaceholder = settings.collapseParams,
+                        )
                     )
-                )
+
+                    seq(*elements.toTypedArray())
+                }
                 is BoundType -> hintFor(type.type, specializations + type.specializations)
             }
         }
 
-        fun InlayPresentation.withPsiReference(type: Type): InlayPresentation {
+        private fun PresentationFactory.genericTypeHints(
+            type: GenericType,
+            specializations: Map<TypeParameter, Type>,
+        ): InlayPresentation {
+            require(type.typeParameters.isNotEmpty())
+
+            return collapsible(
+                prefix = text("<"),
+                collapsed = text(".."),
+                expanded = {
+                    join(
+                        type.typeParameters.map { hintFor(it, specializations) },
+                        separator = { text(", ") },
+                    )
+                },
+                suffix = text(">"),
+                startWithPlaceholder = settings.collapseGenerics,
+            )
+        }
+
+        private fun InlayPresentation.withPsiReference(type: Type): InlayPresentation {
             val target = type.psiElement
             return if (target != null) {
                 return factory.reference(this) {
@@ -184,9 +228,15 @@ class JaktInlayHintsProvider : InlayHintsProvider<JaktInlayHintsProvider.Setting
                 val target = element.expression
                 if (target is JaktPlainQualifierExpression) {
                     val resolved = target.plainQualifier.reference?.resolve()
-                    return if (resolved is JaktEnumVariant || resolved is JaktStructDeclaration) {
-                        target.text == (resolved as PsiNameIdentifierOwner).name
-                    } else false
+                    if (resolved !is JaktEnumVariant && resolved !is JaktStructDeclaration)
+                        return false
+
+                    if (target.text != (resolved as PsiNameIdentifierOwner).name)
+                        return false
+
+                    // If the type is generic, we consider it non-obvious, since the generic type
+                    // may not be obvious from the declaration
+                    return resolved !is JaktGeneric || resolved.getDeclGenericBounds().isEmpty()
                 }
             }
 
@@ -211,6 +261,11 @@ class JaktInlayHintsProvider : InlayHintsProvider<JaktInlayHintsProvider.Setting
                     settings::collapseParams
                 ),
                 ImmediateConfigurable.Case(
+                    "Collapse generic parameter types by default",
+                    "collapse-generics",
+                    settings::collapseGenerics,
+                ),
+                ImmediateConfigurable.Case(
                     "Show \"try\" hints for throwing expressions",
                     "show-try-hints",
                     settings::showTryHints,
@@ -226,6 +281,7 @@ class JaktInlayHintsProvider : InlayHintsProvider<JaktInlayHintsProvider.Setting
         var showForForDecl: Boolean = true,
         var collapseTuples: Boolean = false,
         var collapseParams: Boolean = false,
+        var collapseGenerics: Boolean = false,
         var showTryHints: Boolean = true,
     )
 
