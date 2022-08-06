b3e0030dbf0c91b3a326d3a26d0880a9df97ec22
Matthew Olsson <matthewcolsson@gmail.com>
2022-08-06 11:22:08 -0700

    Add a BreadcrumbsInfoProvider

---
+++ src/main/kotlin/org/serenityos/jakt/structure/JaktBreadcrumbsInfoProvider.kt
@@ -0,0 +1,24 @@
+package org.serenityos.jakt.structure
+
+import com.intellij.psi.PsiElement
+import com.intellij.ui.breadcrumbs.BreadcrumbsProvider
+import org.intellij.sdk.language.psi.*
+import org.serenityos.jakt.JaktLanguage
+import org.serenityos.jakt.render.renderType
+
+class JaktBreadcrumbsInfoProvider : BreadcrumbsProvider {
+    override fun getLanguages() = arrayOf(JaktLanguage)
+
+    override fun acceptElement(element: PsiElement) = getElementInfoImpl(element) != null
+
+    override fun getElementInfo(element: PsiElement) = getElementInfoImpl(element)!!
+
+    private fun getElementInfoImpl(element: PsiElement) = when (element) {
+        is JaktFunction -> element.name + "()"
+        is JaktStructField -> element.name
+        is JaktEnumDeclaration -> renderType(element.jaktType)
+        is JaktStructDeclaration -> renderType(element.jaktType)
+        is JaktNamespaceDeclaration -> "namespace ${element.name}"
+        else -> null
+    }
+}
