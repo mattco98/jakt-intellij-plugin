904d147d21b5c63e69a1733a7d59c90c1fda1f7d
Matthew Olsson <matthewcolsson@gmail.com>
2022-08-07 08:28:17 -0700

    Add DeclarationRangeHandlers

--- src/main/kotlin/org/serenityos/jakt/JaktDeclarationRangeHandler.kt
+++ src/main/kotlin/org/serenityos/jakt/JaktDeclarationRangeHandler.kt
@@ -2,47 +2,38 @@ package org.serenityos.jakt
 
 import com.intellij.codeInsight.hint.DeclarationRangeHandler
 import com.intellij.openapi.util.TextRange
-import org.intellij.sdk.language.psi.JaktEnumDeclaration
-import org.intellij.sdk.language.psi.JaktFunction
-import org.intellij.sdk.language.psi.JaktNamespaceDeclaration
-import org.intellij.sdk.language.psi.JaktStructDeclaration
-import org.serenityos.jakt.psi.JaktPsiElement
+import com.intellij.psi.PsiElement
+import org.serenityos.jakt.psi.api.JaktEnumDeclaration
+import org.serenityos.jakt.psi.api.JaktFunction
+import org.serenityos.jakt.psi.api.JaktNamespaceDeclaration
+import org.serenityos.jakt.psi.api.JaktStructDeclaration
 
-class JaktStructDeclRangeHandler : DeclarationRangeHandler<JaktStructDeclaration> {
-    override fun getDeclarationRange(container: JaktStructDeclaration): TextRange? = with(container) {
-        val start = externKeyword ?: classKeyword ?: structKeyword!!
-        val end = genericBounds ?: identifier
-        TextRange.create(start.textRange.startOffset, end.textRange.endOffset)
+abstract class JaktDeclarationRangeHandler<T : PsiElement> : DeclarationRangeHandler<T> {
+    abstract fun T.start(): PsiElement
+    abstract fun T.end(): PsiElement
+
+    final override fun getDeclarationRange(container: T): TextRange {
+        return TextRange.create(container.start().textRange.startOffset, container.end().textRange.endOffset)
     }
-
 }
 
-class JaktDeclarationRangeHandler : DeclarationRangeHandler<JaktPsiElement> {
-    override fun getDeclarationRange(container: JaktPsiElement): TextRange? = with(container) {
-        val (startEl, endEl) = when (this) {
-            is JaktStructDeclaration -> {
-                val start = externKeyword ?: classKeyword ?: structKeyword!!
-                val end = genericBounds ?: identifier
-                start to end
-            }
-            is JaktEnumDeclaration -> {
-                val start = boxedKeyword ?: enumKeyword
-                val end = underlyingTypeEnumBody?.typeAnnotation
-                    ?: normalEnumBody?.genericBounds
-                    ?: identifier
+class JaktStructDeclRangeHandler : JaktDeclarationRangeHandler<JaktStructDeclaration>() {
+    override fun JaktStructDeclaration.start() = externKeyword ?: classKeyword ?: structKeyword!!
+    override fun JaktStructDeclaration.end() = genericBounds ?: identifier
+}
 
-                start to end
-            }
-            is JaktNamespaceDeclaration -> namespaceKeyword to identifier
-            is JaktFunction -> {
-                val start = externKeyword ?: functionKeyword
-                val end = functionReturnType.let { it.type ?: it.throwsKeyword }
-                    ?: parameterList.parenClose
-                start to end
-            }
-            else -> return null
-        }
+class JaktEnumDeclRangeHandler : JaktDeclarationRangeHandler<JaktEnumDeclaration>() {
+    override fun JaktEnumDeclaration.start() = boxedKeyword ?: enumKeyword
+    override fun JaktEnumDeclaration.end() = underlyingTypeEnumBody?.typeAnnotation
+        ?: normalEnumBody?.genericBounds ?: identifier
+}
 
-        return TextRange.create(startEl.textRange.startOffset, endEl.textRange.endOffset)
-    }
+class JaktNamespaceDeclRangeHandler : JaktDeclarationRangeHandler<JaktNamespaceDeclaration>() {
+    override fun JaktNamespaceDeclaration.start() = namespaceKeyword
+    override fun JaktNamespaceDeclaration.end() = identifier
+}
+
+class JaktFunctionDeclRangeHandler : JaktDeclarationRangeHandler<JaktFunction>() {
+    override fun JaktFunction.start() = externKeyword ?: functionKeyword
+    override fun JaktFunction.end() = functionReturnType.let { it.type ?: it.throwsKeyword } ?: parameterList.parenClose
 }
