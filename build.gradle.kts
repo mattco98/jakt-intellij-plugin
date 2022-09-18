3b54f807d829e4a6644dfad54636bebf5653c76e
Matthew Olsson <matthewcolsson@gmail.com>
2022-09-18 09:51:56 -0700

    Build: Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 plugins {
     idea
     kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.8.1"
+    id("org.jetbrains.intellij") version "1.9.0"
     id("org.jetbrains.grammarkit") version "2021.2.2"
     kotlin("plugin.serialization") version "1.7.10"
 }
@@ -26,7 +26,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
     implementation("net.java.dev.jna:jna:5.12.1")
-    testImplementation("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
+    testImplementation("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
 }
 
 intellij {
